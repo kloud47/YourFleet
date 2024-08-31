3de64c4f720db5e650194b9db105bd9f40735510
Yash Jain <yashj8858@gmail.com>
2024-08-31 23:24:45 +0530

    add CredentialsProvider and fix google auth error

--- src/app/providers/AuthProvider.tsx
+++ src/app/providers/AuthProvider.tsx
@@ -1,9 +1,12 @@
+"use client";
+import React from "react";
 import { SessionProvider } from "next-auth/react";
+const Providers = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <SessionProvider>{children}</SessionProvider>
+    </>
+  );
+};
 
-const AuthProvider = ({ children }: { children: React.ReactNode}) => {
-    return (
-        <SessionProvider session={session}>
-            {children}
-        </SessionProvider>
-    )
-}
+export default Providers;
